fe95006e0441437b327b1e1b931d8420582c01ed
홍수정 <hongsoojeong@hongsujeong-ui-MacBookAir.local>
2022-07-08 16:17:03 +0900

    commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //var leftDiceNumber = 1
+    //var RightDiceNumber = 5
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    // Interface Builder 작업인 IBAction
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image =  diceArray.randomElement()
+     //leftDiceNumber += 1
+        
+        diceImageView2.image =  diceArray.randomElement()
+     //RightDiceNumber -= RightDiceNumber
+        
+        
     }
-
-
+    
 }
 
